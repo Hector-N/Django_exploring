02403cfd4dd835078e4c02ffc705826c2a72673a
Igor Novichenko <Hector.Novichenko@gmail.com>
2020-11-13 17:57:10 +0200

    Add paginator.
    	modified:   wepa/blog/templates/blog/_blog-feed.html
    	modified:   wepa/blog/views.py
    	modified:   wepa/wepa_db.sqlite3

--- wepa/blog/views.py
+++ wepa/blog/views.py
@@ -3,21 +3,33 @@ from django.shortcuts import get_object_or_404, get_list_or_404, render
 from django.template import loader
 from django.urls import reverse
 from .models import Post, Tag, Comment
+from django.core.paginator import Paginator
 
 
 def feed(request, year=None, month=None):
-
     if year and month:
-        posts = Post.objects.filter(date__year=year).filter(date__month=month)
+        posts = Post.objects.\
+                filter(date__year=year).\
+                filter(date__month=month).\
+                order_by('-date')
     elif year:
-        posts = Post.objects.filter(date__year=year)
+        posts = Post.objects.\
+                filter(date__year=year).\
+                order_by('-date')
     else:
-        posts = Post.objects.all()
+        posts = Post.objects.all().order_by('-date')
+
+    paginator = Paginator(object_list=posts,  # required
+                          per_page=3,  # required
+                          orphans=1,  #optional
+                          allow_empty_first_page=True)  # default
+    page_num = request.GET.get('page')
+
+    # ?page = 2  # in the url we can send the page num like this
+    posts_page_obj = paginator.get_page(page_num)
 
     return render(request, 'blog/index.html',
-                  context={'posts': posts,
-                           'all_comments': False,
-                           })
+                  context={'posts': posts_page_obj, 'all_comments': False})
 
 
 def post_details(request, post_id):
@@ -27,7 +39,7 @@ def post_details(request, post_id):
     try:
         # post = Post.objects.get(pk=post_id)
         post_in_list = Post.objects.filter(pk=post_id)
-    except (Post.DoesNotExist):
+    except Post.DoesNotExist:
         raise Http404("Page doesn't exists")
 
     context = {'posts': post_in_list,
@@ -53,11 +65,14 @@ def add_comment(request, post_id):
 
 
 def tag_filter(request, tag_id):
-    # tag = Tag.objects.get(pk=tag_id)
-    tag = get_object_or_404(Tag, pk=tag_id)
+
+    tag = get_object_or_404(klass=Tag, pk=tag_id)
     posts = Post.objects.all().filter(tags__id=tag_id)
+    paginator = Paginator(object_list=posts, per_page=3, orphans=1)
+    page_num = request.GET.get('page')
+    posts_page_obj = paginator.get_page(page_num)
     context = {'search_tag': tag,
-               'posts': posts,
+               'posts': posts_page_obj,
                'all_comments': False}
 
     return render(request, 'blog/index.html', context)
