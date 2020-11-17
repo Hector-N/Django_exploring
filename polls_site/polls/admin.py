a6d4c05ea519e1ed1240b66b1e3dc78a359d2e57
Igor Novichenko <Hector.Novichenko@gmail.com>
2020-11-17 18:00:35 +0200

    Create Question and Option models. Customize admin panel.

--- polls_site/polls/admin.py
+++ polls_site/polls/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Option
+
+
+# class OptionInline(admin.StackedInline):
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 3
+
+
+class OptionAdmin(admin.ModelAdmin):
+    # fields
+    # fieldsets
+    list_display = ['text', 'question_id', 'votes']
+    list_filter = ['question_id', 'votes']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['text', 'date']
+    list_filter = ['date']
+    search_fields = ['text']
+    inlines = [OptionInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Option, OptionAdmin)
--- polls_site/polls/models.py
+++ polls_site/polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+
+    text = models.CharField(max_length=300)
+    date = models.DateTimeField()
+
+    def __str__(self):
+
+        return self.text
+
+
+class Option(models.Model):
+    text = models.CharField(max_length=100)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+
+        return self.text
