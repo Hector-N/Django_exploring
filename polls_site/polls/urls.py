99a62cc255673856289cc57640defd77c75ca1ab
Igor Novichenko <Hector.Novichenko@gmail.com>
2020-11-17 15:36:37 +0200

    Create html templates and static css.

--- polls_site/polls/urls.py
+++ polls_site/polls/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home
-
+# from .views import home
+from . import views
 
 app_name = 'polls'
 
 urlpatterns = [
-    path('', home, name='homepage'),
+    path('', views.home, name='homepage'),
+    path('poll<int:poll_num>/', views.show_poll, name='details'),
+    path('poll<int:poll_num>/vote/', views.vote_poll, name='vote'),
 ]
--- polls_site/polls/views.py
+++ polls_site/polls/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def home(request):
-    return HttpResponse("HOME PAGE")
+    template = loader.get_template('polls/index.html')
+    return HttpResponse(template.render({"context": 'Hello!'}))
+
+
+
+def show_poll(request, poll_num):
+    return render(request, 'polls/poll_details.html', {'context': 'boo!'})
+
+
+def vote_poll(request, poll_num):
+    return HttpResponse(f"voting on poll number {poll_num}")
+
+
+
